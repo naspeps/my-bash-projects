7364b625f4bedec048f072e7dc882aad6152bf0e
Nasser Pepouna <naspeps@gmail.com>
2022-07-09 11:39:49 +0200

    added rsync_test.sh

--- rsync_test.sh
+++ rsync_test.sh
@@ -1,1 +1,27 @@
-foo
+#!/bin/bash
+
+EMAILHOST="user@server"
+DIR="/directory/path/to/the/files/"
+DEST="${EMAILHOST}:/destination/path/in/email/server/"
+RETRY=false
+
+#scan DIR and create a temporal file(FILE_LIST) containing names of files in it
+#temporal file is deleted once script is done
+FILE_LIST=$(mktemp)
+find "$DIR" -maxdepth 1 -type f -printf "%f\n" > "$FILE_LIST"
+trap 'rm -f "$FILE_LIST"' EXIT
+
+while true; do
+        SUCCESS=true
+        rsync --rsync-path="sudo rsync" \
+                --archive \
+                --compress \
+                --files-from="$FILE_LIST" \
+                ${DIR} ${DEST} || SUCCESS=false
+        $SUCCESS && break;
+        if ! $RETRY; then
+                echo "Rsync not possible" >&2
+                exit 1
+        fi
+done
+echo "transfer successfull"
